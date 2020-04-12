db8f8f75383a124561f7371630b1606ff55e3396
mike <1541992+mike4192@users.noreply.github.com>
2020-04-12 10:52:19 -0400

    more spot micro kinematic work. Added readme with conda install notes

--- spot_micro_project_notes/spot_micro_kinematics/utilities/spot_micro_kinematics.py
+++ spot_micro_project_notes/spot_micro_kinematics/utilities/spot_micro_kinematics.py
@@ -1,5 +1,5 @@
 from . import transformations
-from math import pi
+from math import pi, cos, sin
 import numpy as np
 
 def t_rightback(t_m,l,w):
@@ -20,7 +20,7 @@ def t_rightback(t_m,l,w):
                                     [np.array([0,0,0,1])] ]    )
     return t_m @ temp_homog_transf
 
-def rightfront(t_m,l,w):
+def t_rightfront(t_m,l,w):
     '''Creates a 4x4 numpy homogeneous transformation matrix representing coordinate system and 
     position of the rightfront leg of a quadriped. Assumes legs postioned in corners of a rectangular
     plane defined by a width and length 
@@ -73,3 +73,39 @@ def t_leftback(t_m,l,w):
     temp_homog_transf = np.block( [ [ transformations.roty(-pi/2), np.array([[-l/2],[0],[-w/2]])  ],
                                     [np.array([0,0,0,1])] ]    )
     return t_m @ temp_homog_transf
+
+
+def t_0_to_1(theta1,l1):
+    '''Create the homogeneous transformation matrix for joint 0 to 1 for a quadriped leg.
+
+    Args:
+        theta1: Rotation angle in radians of the hip joint
+        l1: Length of the hip joint link
+
+    Returns:
+        A 4x4 numpy matrix. Homogeneous transform from joint 0 to 1
+    '''
+    # I believe there is a typo in the paper. The paper lists this matrix as:
+    # 
+    # t =    [ cos(theta1)     -sin(theta1)    0        -l1*cos(theta1);
+    #          sin(theta1)     -cos(theta1)    0        -l1*sin(theta1);
+    #                    1                0    0                      0;
+    #                    0                0    0                      1;]
+    # 
+    # However I believe index [2],[0] should be zero, and index [2],[2] should be 1 instead.
+    # If not, then the rotated z axis disapears?? And from the diagram, it appears the transformed
+    # axis's z axis is the same as the original. So I think the matrix should be:
+    # 
+    # t =    [ cos(theta1)     -sin(theta1)    0        -l1*cos(theta1);
+    #          sin(theta1)     -cos(theta1)    0        -l1*sin(theta1);
+    #                    0                0    1                      0;
+    #                    0                0    0                      1;]
+    
+    t_01 = np.block( [ [ transformations.rotz(theta1), np.array([[-l1*cos(theta1)],[-l1*sin(theta1)],[0]])  ],
+                                    [np.array([0,0,0,1])] ]    )
+
+    
+    return t_01
+
+
+
--- spot_micro_project_notes/spot_micro_kinematics/utilities/tests/test_spot_micro_kinematics.py
+++ spot_micro_project_notes/spot_micro_kinematics/utilities/tests/test_spot_micro_kinematics.py
@@ -10,19 +10,20 @@ class TestSpotMicroKinematics(unittest.TestCase):
     '''Tests rotation matrices'''
 
     def test_t_rightback(self):
-        '''Test the homogeneous transformation via a contrived example'''
+        '''Test the homogeneous transformation to the right back leg via a contrived example'''
         
         # First create a homogenous transformation representing the robot center body.
         # No rotations, no translations
 
         t_m = transformations.homog_transform(0,0,0,0,0,0)
-
-        t_rightback = spot_micro_kinematics.t_rightback(t_m,2,3)
+        l = 2
+        w = 3
+        t_rightback = spot_micro_kinematics.t_rightback(t_m,l,w)
 
         known_true_rot = np.array(
-            [   [cos(pi/2),   0,  sin(pi/2),  -2/2],
+            [   [cos(pi/2),   0,  sin(pi/2),  -l/2],
                 [0,           1,          0,     0],
-                [-sin(pi/2),  0,  cos(pi/2),   3/2],
+                [-sin(pi/2),  0,  cos(pi/2),   w/2],
                 [0,           0,          0,     1]    ])
 
         try:
@@ -33,3 +34,100 @@ class TestSpotMicroKinematics(unittest.TestCase):
             print (err)
         self.assertTrue(res)
         
+
+    def test_t_rightfront(self):
+        '''Test the homogeneous transformation to the right front leg via a contrived example'''
+        
+        # First create a homogenous transformation representing the robot center body.
+        # No rotations, no translations
+
+        t_m = transformations.homog_transform(0,0,0,0,0,0)
+        l = 2
+        w = 3
+        t_rightfront = spot_micro_kinematics.t_rightfront(t_m,l,w)
+
+        known_true_rot = np.array(
+            [   [cos(pi/2),   0,  sin(pi/2),   l/2],
+                [0,           1,          0,     0],
+                [-sin(pi/2),  0,  cos(pi/2),   w/2],
+                [0,           0,          0,     1]    ])
+
+        try:
+            np.testing.assert_array_almost_equal(t_rightfront, known_true_rot)
+            res = True
+        except AssertionError as err:
+            res = False
+            print (err)
+        self.assertTrue(res)
+        
+    def test_t_leftfront(self):
+        '''Test the homogeneous transformation to the left front leg via a contrived example'''
+        
+        # First create a homogenous transformation representing the robot center body.
+        # No rotations, no translations
+
+        t_m = transformations.homog_transform(0,0,0,0,0,0)
+        l = 2
+        w = 3
+        t_leftfront = spot_micro_kinematics.t_leftfront(t_m,l,w)
+
+        known_true_rot = np.array(
+            [   [cos(-pi/2),   0,  sin(-pi/2),   l/2],
+                [0,           1,          0,     0],
+                [-sin(-pi/2),  0,  cos(-pi/2),   -w/2],
+                [0,           0,          0,     1]    ])
+
+        try:
+            np.testing.assert_array_almost_equal(t_leftfront, known_true_rot)
+            res = True
+        except AssertionError as err:
+            res = False
+            print (err)
+        self.assertTrue(res)
+
+    def test_t_leftback(self):
+        '''Test the homogeneous transformation to the left back leg via a contrived example'''
+        
+        # First create a homogenous transformation representing the robot center body.
+        # No rotations, no translations
+
+        t_m = transformations.homog_transform(0,0,0,0,0,0)
+        l = 2
+        w = 3
+        t_leftback = spot_micro_kinematics.t_leftback(t_m,l,w)
+
+        known_true_rot = np.array(
+            [   [cos(-pi/2),   0,  sin(-pi/2),   -l/2],
+                [0,           1,          0,     0],
+                [-sin(-pi/2),  0,  cos(-pi/2),   -w/2],
+                [0,           0,          0,     1]    ])
+
+        try:
+            np.testing.assert_array_almost_equal(t_leftback, known_true_rot)
+            res = True
+        except AssertionError as err:
+            res = False
+            print (err)
+        self.assertTrue(res)
+
+    def test_t_0_to_1(self):
+        '''Test the homogeneous transformation of leg joint 0 to joint 1 via a contrived example'''
+        
+        theta1 = 35*pi/180
+        l1 = 0.4
+        t = spot_micro_kinematics.t_0_to_1(theta1,l1)
+
+        known_true = np.array(
+            [   [cos(theta1),  -sin(theta1),             0,   -l1*cos(theta1)],
+                [sin(theta1),   cos(theta1),             0,   -l1*sin(theta1)],
+                [          0,             0,             1,                 0],
+                [          0,             0,             0,                 1]    ])
+
+        try:
+            np.testing.assert_array_almost_equal(t, known_true)
+            res = True
+        except AssertionError as err:
+            res = False
+            print (err)
+        self.assertTrue(res)
+    
